a27f385ab3ca0b5d9f155fbcdf535a62f17aab04
Akira Kawata <akirakawata1@gmail.com>
2024-08-24 08:50:53 +0900

    Delete source code from glibc

--- ch02_elf/09_symbol_table/gnu_hash.c
+++
@@ -1,7 +0,0 @@
-static uint_fast32_t dl_new_hash(const char *s) {
-  uint_fast32_t h = 5381;
-  for (unsigned char c = *s; c != '\0'; c = *++s) {
-    h = h * 33 + c;
-  }
-  return h & 0xffffffff;
-}
