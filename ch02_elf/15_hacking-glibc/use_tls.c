54bceaa0958e8308fdf6902b65e6b35a0551a848
Akira Kawata <akawashiro@users.noreply.github.com>
2024-08-24 11:49:23 +0900

    Update ch02_elf/15_hacking-glibc/use_tls.c
    Co-authored-by: arata, mizuki <minorinoki@gmail.com>

--- ch02_elf/15_hacking-glibc/use_tls.c
+++ ch02_elf/15_hacking-glibc/use_tls.c
@@ -1,4 +1,3 @@
 #include <stdio.h>
-#include <threads.h>
-thread_local unsigned long tls_var_with_init = 0xaabbccddaabbccdd;
+_Thread_local unsigned long tls_var_with_init = 0xaabbccddaabbccdd;
 int main(void) { printf("tls_var_with_init = 0x%lx\n", tls_var_with_init); }
